2a1b69ae08a3b4202c9d28820c02fc8388d570b6
luyu <yu.lu@ucdconnect.ie>
2022-07-19 14:19:12 +0800

    add logo

--- source/conf.py
+++ source/conf.py
@@ -61,4 +61,4 @@ html_title =""
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-html_logo = "logo_image/qlib3.png"
+html_logo = "logo_image/Qlib.png"
